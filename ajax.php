939f7dccbcf7c2205ce8eb15e246e71943d40cff
Miguel Ángel <maletils@gmail.com>
2017-09-03 23:11:58 +0200

    Ajax con búsqueda, #4. Fixed #2

--- ajax.php
+++ ajax.php
@@ -13,28 +13,11 @@ if (isset($_GET["auth"])){
 <html>
 <head>
     <script>
-
-/*        function orderby(str) {
-            if (str.length==0 | str.length==1) {
-                document.getElementById("livesearch").innerHTML="";
-                document.getElementById("livesearch").style.border="0px";
-                return;
-            }
-            if (window.XMLHttpRequest) {
-                // code for IE7+, Firefox, Chrome, Opera, Safari
-                xmlhttp=new XMLHttpRequest();
-            } else {  // code for IE6, IE5
-                xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
-            }
-            xmlhttp.onreadystatechange=function() {
-                if (this.readyState==4 && this.status==200) {
-                    document.getElementById("livesearch").innerHTML=this.responseText;
-                    var order = str;
-                }
-            };
+        var order = "";
+        function orderby(str) {
             console.log(str);
-            var order = str;
-        }*/
+            order = str;
+        }
         function showResult(str) {
             if (str.length==0 | str.length==1) {
                 document.getElementById("livesearch").innerHTML="";
@@ -62,7 +45,7 @@ if (isset($_GET["auth"])){
             }else {
                 $url = "http://localhost/GsoftWEB/articulos.php?auth=". $auth ."&getPrice=false&search="; } ?>
             console.log(order);
-            xmlhttp.open("GET","<?php echo $url;?>"+ str + "&orderby=" + order,true);
+            xmlhttp.open("GET","<?php echo $url;?>"+ str + "&orderBy=" + order,true);
             xmlhttp.send();
             console.log(xmlhttp);
         }
