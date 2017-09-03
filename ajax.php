b555df963203ff83c3cea4cfae6bc77a04892ac3
Miguel Ángel <maletils@gmail.com>
2017-09-03 21:21:46 +0200

    Pruebas de ajax con doble entrada

--- ajax.php
+++ ajax.php
@@ -13,6 +13,28 @@ if (isset($_GET["auth"])){
 <html>
 <head>
     <script>
+
+/*        function orderby(str) {
+            if (str.length==0 | str.length==1) {
+                document.getElementById("livesearch").innerHTML="";
+                document.getElementById("livesearch").style.border="0px";
+                return;
+            }
+            if (window.XMLHttpRequest) {
+                // code for IE7+, Firefox, Chrome, Opera, Safari
+                xmlhttp=new XMLHttpRequest();
+            } else {  // code for IE6, IE5
+                xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+            }
+            xmlhttp.onreadystatechange=function() {
+                if (this.readyState==4 && this.status==200) {
+                    document.getElementById("livesearch").innerHTML=this.responseText;
+                    var order = str;
+                }
+            };
+            console.log(str);
+            var order = str;
+        }*/
         function showResult(str) {
             if (str.length==0 | str.length==1) {
                 document.getElementById("livesearch").innerHTML="";
@@ -31,15 +53,16 @@ if (isset($_GET["auth"])){
                 }
             };
 
-            <?php $mode = 0; $orderBy = "Familia";
+            <?php $mode = 0;
             if (isset($_GET["mode"])){
                 $mode = $_GET["mode"];
             }
             if ($mode == 1){
                 $url = "http://localhost/GsoftWEB/clientes.php?auth=1&search=";
             }else {
-                $url = "http://localhost/GsoftWEB/articulos.php?auth=". $auth ."&orderBy=". $orderBy ."&getPrice=false&search="; } ?>
-            xmlhttp.open("GET","<?php echo $url;?>"+str,true);
+                $url = "http://localhost/GsoftWEB/articulos.php?auth=". $auth ."&getPrice=false&search="; } ?>
+            console.log(order);
+            xmlhttp.open("GET","<?php echo $url;?>"+ str + "&orderby=" + order,true);
             xmlhttp.send();
             console.log(xmlhttp);
         }
@@ -47,25 +70,13 @@ if (isset($_GET["auth"])){
 </head>
 <body>
     <form>
-        <input type="radio" id="nombre" name="order" value="nombre" checked>
+        <input type="radio" id="nombre" name="order" value="Nombre" onclick="orderby(this.value);"  checked>
         <label for="nombre">Nombre</label>
-        <input type="radio" id="familia" name="order" value="familia">
+        <input type="radio" id="familia" name="order" value="Familia" onclick="orderby(this.value);">
         <label for="familia">Familia</label>
-        <input type="radio" id="fecha" name="order" value="fecha">
+        <input type="radio" id="fecha" name="order" value="Fecha" onclick="orderby(this.value);">
         <label for="fecha">Fecha</label>
     </form>
-    <script>
-
-
-        var x = document.getElementById("nombre").value;
-        if (x = "nombre"){
-            console.log("nombre");
-        } else {
-            console.log("no es nombre");
-        }
-    
-
-</script>
 <form>
 <label>
 <input type="text" size="30" onkeyup="showResult(this.value)">
